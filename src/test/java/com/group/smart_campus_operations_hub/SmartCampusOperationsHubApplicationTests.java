8d35f3e5f6dbc1808e9dfe8a60b0f7a6fe31f5e2
Ranahasuni <pererachathurya05@gmail.com>
2026-04-03 13:17:38 +0530

    initial spring boot project

---
+++ src/test/java/com/group/smart_campus_operations_hub/SmartCampusOperationsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.smart_campus_operations_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusOperationsHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
