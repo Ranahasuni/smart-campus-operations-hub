f88ae6d96389c3f62e95d0147cd6409875a7b211
asanjalee <asanjalee456@gmail.com>
2026-04-07 21:36:35 +0530

    feat: add findByTicketId to CommentRepository

--- backend/src/main/java/com/smartcampus/repository/CommentRepository.java
+++ backend/src/main/java/com/smartcampus/repository/CommentRepository.java
@@ -4,7 +4,9 @@ import com.smartcampus.model.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends MongoRepository<Comment, String> {
-    // Empty skeleton!
+    List<Comment> findByTicketId(String ticketId);
 }
