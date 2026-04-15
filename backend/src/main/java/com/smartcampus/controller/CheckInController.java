dac15d94b855f67893138fe422ed3ecdd8060ecb
Ranahasuni <pererachathurya05@gmail.com>
2026-04-15 18:17:11 +0530

    fix(api): Correct duplicated headers and imports in CheckInController

--- backend/src/main/java/com/smartcampus/controller/CheckInController.java
+++ backend/src/main/java/com/smartcampus/controller/CheckInController.java
@@ -1,20 +1,4 @@
 package com.smartcampus.controller;
-+
-+import com.smartcampus.model.Booking;
-+import com.smartcampus.model.BookingStatus;
-+import com.smartcampus.repository.BookingRepository;
-+import lombok.RequiredArgsConstructor;
-+import org.springframework.http.ResponseEntity;
-+import org.springframework.web.bind.annotation.*;
-+
-+import java.time.LocalDate;
-+import java.time.LocalDateTime;
-+import java.time.LocalTime;
-+import java.util.Map;
-+
-+@RestController
-+@RequestMapping("/api/check-in")
-+@RequiredArgsConstructor
 
 import com.smartcampus.model.Booking;
 import com.smartcampus.model.BookingStatus;
@@ -25,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Map;
 
 @RestController
